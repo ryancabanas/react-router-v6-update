6df2b6a8b91bea48dafb8c475aec548b2d6eff34
Ryan Cabanas <ryangithub777@gmail.com>
2022-08-28 23:04:27 -0400

    Lesson #291:
    - An alternative way to handle nested `Route` components is to place them
      in the same component file where the top level `Route` component exists.
      You would just structure it like a child element.
    
      Note that the `path` property still requires a ***relative*** value,
      even though the `Route` component now sits in the same file as the
      parent `Route` component.
    
    - In the downstream component, you still need to tell react router where
      to display the nested `Route` component's content.  You use the `Outlet`
      component for this now.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,13 +1,11 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
       <Link to="new-user">New User</Link>
-      <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
+      <Outlet />
     </section>
   );
 };
